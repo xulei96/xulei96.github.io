71d0c9b594e37ebfbaf76d09ee0a193c14a821da
xulei96 <31477877@qq.com>
2017-09-11 17:27:01 +0800

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,41 @@
+window.onload=function () {
+	myScroll()
+	$(window).scroll(function () {
+		myScroll()
+	});
+	$(".m-development").hover(function () {
+		$(this).children(".hidden-bg").stop().animate({
+			opacity: "0"
+		},200)
+	},function () {
+		$(this).children(".hidden-bg").stop().animate({
+			opacity: "1"
+		},200)
+	})
+	/*点击播放音乐*/
+	$(".sound-toggle.is-visible").click(function () {
+		if ($(this).hasClass("is-playing")) {
+			$(this).removeClass("is-playing")
+			$("#sound-music")[0].pause()
+		}else{
+			$(this).addClass("is-playing")
+			$("#sound-music")[0].play()
+		}
+//		$(this).removeClass("is-visible")
+	})
+//	$(".sound-toggle.is-visible").click(function () {
+//		$(this).removeClass("is-playing")
+////		$(this).addClass("is-visible")
+//	})
+}
+function myScroll() {
+	if ($(window).scrollTop()>50) {
+		$(".m-bg").stop().animate({
+			opacity : '0'
+		},300)
+	}else {
+		$(".m-bg").stop().animate({
+			opacity : '1'
+		},300)
+	}
+}
